63c14475737a8dba7680b5d6350f9a293d5b03cb
Eren YILMAZ <erenyilmazgazi@gmail.com>
2020-08-15 20:22:00 +0300

    asp net core denemesi

---
+++ Jira-Project/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using Newtonsoft.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Controller]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var client = new RestClient("http://localhost:61775/api/api");  //LOCALDEKİ REST API
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET) { RequestFormat = DataFormat.Json };  //POST REQUEST ATILACAK
+            var response = client.Execute(request);  //REQUESTİ EXECUTE ET
+
+
+
+            List<Product> Products = JsonConvert.DeserializeObject<List<Product>>(response.Content); //NESNEYE ÇEVİR
+            Product product1 = new Product();
+            return View();
+        }
+    }
+}
---
+++ Jira-Project/WebApplication1/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int productID { get; set; }
+        public string productName { get; set; }
+        public int categoryID { get; set; }
+        public int companyID { get; set; }
+        public decimal price { get; set; }
+        public string quantityPerUnit { get; set; }
+    }
+}
